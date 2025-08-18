4b195cd4a5d9fa2f48a95ff6f1de348590a9a8c7
lenovo <lenovo@DESKTOP-8L1R360.lan>
2025-08-18 19:27:58 +0530

    Increment of employees

---
+++ java_program_assignments/src/employees/Developer.java
@@ -0,0 +1,27 @@
+package employees;
+
+
+public class Developer extends Employees {
+	private String programlang;
+	
+	public Developer(String name,String employeeId,double salary,String programlang) {
+		super(name,employeeId,salary);
+		this.programlang=programlang;
+	}
+
+	
+	public String getProgramlang() {
+		return programlang;
+	}
+
+	public void setProgramlang(String programlang) {
+		this.programlang = programlang;
+	}
+	@Override
+
+	public String dis(){
+		return "Developer [programlang=" + programlang + "]";
+	}
+	
+
+}
